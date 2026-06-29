3960da2c46cf5e7814f7facb2156e29cd4b330ca
sadik111222 <md8413226@gmail.com>
2026-06-29 16:25:45 +0600

    services page add

---
+++ src/app/services/page.jsx
@@ -0,0 +1,15 @@
+
+import PlatformBenefits from '@/components/PlatformBenefits';
+import React from 'react';
+
+const servicesPage = () => {
+    return (
+        <div>
+            
+            <PlatformBenefits></PlatformBenefits>
+
+        </div>
+    );
+};
+
+export default servicesPage;
